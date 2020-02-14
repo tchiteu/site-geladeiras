1922fa4249a300db31747f7998f9daf5f04fb72f
tchiteu <matheussantos.contato362@gmail.com>
2020-02-13 22:10:40 -0300

    Adicionada conexão com banco de dados OT-09

--- src/br/com/coldigogeladeiras/rest/MarcaRest.java
+++ src/br/com/coldigogeladeiras/rest/MarcaRest.java
@@ -15,20 +15,21 @@ import br.com.coldigogeladeiras.jdbc.JDBCMarcaDAO;
 import br.com.coldigogeladeiras.modelo.Marca;
 
 @Path("marca")
-public class MarcaRest {
+public class MarcaRest extends UtilRest {
 	
 	@GET
 	@Path("/buscar")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response buscar(){		
 		try {
-		List<Marca> listaMarcas = new ArrayList<Marca>();
-			
-		Conexao conec = new Conexao();
-		Connection conexao = conec.abrirConexao();
-		JDBCMarcaDAO jdbcMarca = new JDBCMarcaDAO(conexao);
-		listaMarcas = jdbcMarca.buscar();
-		conec.fecharConexao();	
+			List<Marca> listaMarcas = new ArrayList<Marca>();
+				
+			Conexao conec = new Conexao();
+			Connection conexao = conec.abrirConexao();
+			JDBCMarcaDAO jdbcMarca = new JDBCMarcaDAO(conexao);
+			listaMarcas = jdbcMarca.buscar();
+			conec.fecharConexao();	
+			return this.buildResponse(listaMarcas);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return this.buildErrorResponse(e.getMessage());
